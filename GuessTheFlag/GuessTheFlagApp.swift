d1910102b87eb1147efddeb10d3fe54eeb48b1a7
Ian Bailey <IanKulin@kulin.com.au>
2022-08-22 19:15:02 +0800

    Initial

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ian Bailey on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
